e4ae0ba03b3797489639c18e7b841e29fe0028d3
AhlamFadhl <ahlam34117@gmail.com>
2025-06-25 17:57:54 +0300

    re write routes in web with group

--- routes/web.php
+++ routes/web.php
@@ -11,31 +11,36 @@ Route::get('/dashboard', function () {
     return view('admin.dashboard');
 });
 
-
+Route::get('/login',fn () => 'please login first')->name('login');
 //Student 
 // Student Dashboard
-Route::get(
-    '/student/dashboard',
-    function () {
-        return view('student.dashboard');
-    }
-)->name('student.dashboard');
-// Student Registration
-Route::get(
-    '/student-register',
-    function () {
-        return view('student-register');
-    }
-)->name('student-register');
-Route::post(
-    '/register-student',
-    [StudentsController::class, 'store']
-)->name('register-student');
-//Student Login
-Route::get('/student-login', function () {
-    return view('student-login');
-})->name('student.login');
-Route::post(
-    '/login-student',
-    [StudentsController::class, 'login_student']
-)->name('login-student');
+Route::prefix('student')->group(function () {
+    Route::middleware('auth:customer')->group(function () {
+        Route::get(
+            '/student/dashboard',
+            function () {
+                return view('student.dashboard');
+            }
+        )->name('student.dashboard');
+    });
+
+    // Student Registration
+    Route::get(
+        '/student-register',
+        function () {
+            return view('student-register');
+        }
+    )->name('student-register');
+    Route::post(
+        '/register-student',
+        [StudentsController::class, 'store']
+    )->name('register-student');
+    //Student Login
+    Route::get('/student-login', function () {
+        return view('student-login');
+    })->name('student.login');
+    Route::post(
+        '/login-student',
+        [StudentsController::class, 'login_student']
+    )->name('login-student');
+});
