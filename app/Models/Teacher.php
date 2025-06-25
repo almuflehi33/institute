729f83b10709e4904f51aedf69165a00a5d4fe1f
SabreenHussien <sabreena.sakura@gmail.com>
2025-06-25 18:15:52 +0300

    [Register and Login Teacher]

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -4,9 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Teacher extends Model
+class Teacher extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\TeachersFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'phone',
+        'qualification',
+        'subjects',
+        'gender',
+        'notes',
+    ];
+
+    protected $hidden =['password'];
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\StudentsController;
+use App\Http\Controllers\TeachersController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -42,5 +43,39 @@ Route::prefix('student')->group(function () {
     Route::post(
         '/login',
         [StudentsController::class, 'login_student']
+    )->name('login');
+});
+
+
+//Teacher 
+// Teacher Dashboard
+Route::prefix('teacher')->group(function () {
+    Route::middleware('auth:teacher')->group(function () {
+        Route::get(
+            '/dashboard',
+            function () {
+                return view('teacher.dashboard');
+            }
+        )->name('teacher.dashboard');
+    });
+
+    // Teacher Registration
+    Route::get(
+        '/register',
+        function () {
+            return view('teacher-register');
+        }
+    )->name('register');
+    Route::post(
+        '/register',
+        [TeachersController::class, 'store']
+    )->name('register');
+    //Teacher Login
+    Route::get('/login', function () {
+        return view('teacher-login');
+    })->name('login');
+    Route::post(
+        '/login',
+        [TeachersController::class, 'login_teacher']
     )->name('login');
 });
